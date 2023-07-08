cfb2c49182570a74dd17637eb95e3062c674e624
Akixis <ae19042@shibaura-it.ac.jp>
2023-07-08 16:12:02 +0900

    修正

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         Strage
     </x-slot>
-        <h1>ES_Storage</h1>
+        <p class="Success">ES_Storage</p>
         
         <div class='primary'>
             @foreach ($inds as $ind)
@@ -13,11 +13,5 @@
                 </div>
             @endforeach
         </div>
-        <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+        
 </x-app-layout>
